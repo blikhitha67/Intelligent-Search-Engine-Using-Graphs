5cfc0135cd5dda429e092e01881e033fe533da27
blikhitha67 <s_basina@u.pacific.edu>
2025-12-31 15:10:46 -0800

    Create Processing.py

---
+++ src/Processing.py
@@ -0,0 +1,18 @@
+import re
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def clean_text(text):
+    """
+    Cleans and normalizes text.
+    - Lowercase
+    - Remove special characters
+    - Tokenize & lemmatize
+    - Remove stopwords
+    """
+    text = text.lower()
+    text = re.sub(r"[^a-z\s]", "", text)
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if not token.is_stop]
+    return " ".join(tokens)
