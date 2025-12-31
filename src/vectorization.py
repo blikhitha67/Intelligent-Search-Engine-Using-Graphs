fc0f58479d89e1558193482288bd3de779c275fc
blikhitha67 <s_basina@u.pacific.edu>
2025-12-31 15:15:02 -0800

    Create vectorization.py

---
+++ src/vectorization.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def vectorize_documents(docs):
+    """
+    Convert documents to TF-IDF vectors.
+    Returns vectorizer and tfidf_matrix
+    """
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(docs)
+    return vectorizer, tfidf_matrix
